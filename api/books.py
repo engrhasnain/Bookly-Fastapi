83bc4610c059a949b6e5706e3c94d0fe3952565c
engrhasnain <pirzadahasnain.18@gmail.com>
2026-01-17 23:33:20 +0500

    Add the get_book

--- api/books.py
+++ api/books.py
@@ -1,5 +1,6 @@
 from db.db import books_date
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.exception_handlers import HTTPException
 
 
 router = APIRouter(prefix="/books", tags=["Book CRUD"])
@@ -12,9 +13,21 @@ from typing import List
 async def get_books():
     return books_date
 
-@router.post("/create", response_model=BookResponse)
+@router.post("/create", response_model=BookResponse, status_code=status.HTTP_201_CREATED)
 async def create_book(book: BookCreated):
     new_book = book.model_dump()
     new_book["id"] = len(books_date) + 1
     books_date.append(new_book)
     return BookResponse(**new_book)
+
+
+@router.post("/get_book/{book_id}", response_model= BookResponse, status_code=status.HTTP_302_FOUND)
+async def get_book(book_id:int):
+    for book in books_date:
+        if book["id"] == book_id:
+            return book
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book not found"
+        )
